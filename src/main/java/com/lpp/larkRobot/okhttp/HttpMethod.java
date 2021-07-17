fca7ece8912baff520d74c1127b0e996f785941b
lpp <panpan.li@okg.com>
2021-07-17 22:24:58 +0800

    lark应用机器人demo

--- src/main/java/com/lpp/larkRobot/okhttp/HttpMethod.java
+++ src/main/java/com/lpp/larkRobot/okhttp/HttpMethod.java
@@ -1,10 +1,6 @@
 package com.lpp.larkRobot.okhttp;
 
-/**
- * @author Mr.Liang
- * @description
- * @create 2019-08-15-13:02
- **/
+
 public class HttpMethod {
 
     public static final String GET = "GET";
--- src/main/java/com/lpp/larkRobot/okhttp/MediaTypeConstant.java
+++ src/main/java/com/lpp/larkRobot/okhttp/MediaTypeConstant.java
@@ -2,11 +2,7 @@ package com.lpp.larkRobot.okhttp;
 
 import okhttp3.MediaType;
 
-/**
- * @author Mr.Liang
- * @description
- * @create 2019-08-15-14:10
- **/
+
 public class MediaTypeConstant {
 
     public static final MediaType APPLICATION_JSON = MediaType.parse("application/json; charset=UTF-8");
